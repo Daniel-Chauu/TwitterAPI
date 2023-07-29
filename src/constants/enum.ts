b0d8d3c57d65386a346f4f729773b729d58c1503
ChauQuangkiet <qkjet1311@gmail.com>
2023-07-29 07:35:31 +0700

    Thêm ErrorHandler để xử lí lỗi chuẩn hơn và tạo một wrapper request để try catch cả app

---
+++ src/constants/enum.ts
@@ -0,0 +1,12 @@
+export enum UserVerifyStatus {
+  Unverified, // 0
+  Verified, // 1
+  Banned // 2
+}
+
+export enum TokenType {
+  AccessToken, // 0
+  RefreshToken, // 1
+  ForgotPasswordToken, // 2
+  EmailVerifyToken // 3
+}
